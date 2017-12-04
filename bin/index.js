3c701e9d603c0989bbde9cc93b5edf6d6ed92884
Falco Nogatz <fnogatz@gmail.com>
2017-12-04 16:17:26 +0100

    Use JavaScript Standard Style

--- bin/index.js
+++ bin/index.js
@@ -1,71 +1,71 @@
-'use strict';
+'use strict'
 
-const async = require('async');
-const pr = require('path').resolve;
-const fs = require('fs');
-const _ = require('lodash');
+const async = require('async')
+const pr = require('path').resolve
+const fs = require('fs')
+const _ = require('lodash')
 
-const pdf = require('./lib/card-generator')();
+const pdf = require('./lib/card-generator')()
 
-const SRC_DIR = pr(__dirname, '../src');
-const DATA_DIR = pr(SRC_DIR, 'data');
-const DEST_DIR = pr(__dirname, '../dist');
+const SRC_DIR = pr(__dirname, '../src')
+const DATA_DIR = pr(SRC_DIR, 'data')
+const DEST_DIR = pr(__dirname, '../dist')
 
-const ALPHABET = 'ABCDEFGHIJKMNOPQRSTUVWXYZ';
+const ALPHABET = 'ABCDEFGHIJKMNOPQRSTUVWXYZ'
 
 try {
-  fs.mkdirSync(DEST_DIR);
+  fs.mkdirSync(DEST_DIR)
 } catch (e) {
-  if (e.code !== 'EEXIST') throw e;
+  if (e.code !== 'EEXIST') throw e
 }
 
-let stations = JSON.parse(fs.readFileSync(pr(DATA_DIR, 'stations.json')));
+let stations = JSON.parse(fs.readFileSync(pr(DATA_DIR, 'stations.json')))
 let categories = [
   {
     name: 'Anzahl der Bahnsteige',
     find: station => station.platforms.length,
-    reverse: true,
+    reverse: true
   },
   {
     name: 'Längster Bahnsteig',
     find: station => _(station.platforms).map('length').max(),
     format: n => n.toFixed(2).replace('.', ',') + ' m',
-    reverse: true,
+    reverse: true
   },
   {
     name: 'Höchster Bahnsteig',
     find: station => _(station.platforms).map('height').max(),
     format: n => n.toFixed(2).replace('.', ',') + ' m',
-    reverse: true,
+    reverse: true
   },
   {
     name: 'Bahnhofskategorie',
-    find: n => n.category,
+    find: n => n.category
   },
   {
     name: 'Anzahl der Aufzüge',
     find: n => n.elevators.length,
-    reverse: true,
+    reverse: true
   },
   {
     name: 'Ältester Aufzug',
     find: n => _(n.elevators).map(e => +e.year).filter().min(),
     filter: n => n.elevators.length,
-    format: n => n === Infinity ? '—' : n,
+    format: n => n === Infinity ? '—' : n
   },
   {
     name: 'Größte Aufzugskabine',
     find: n => _(n.elevators).map(e => e.cabin.width * e.cabin.depth * e.cabin.height / 1e9).filter(n => n < 100).max(),
     reverse: true,
-    format: n => (n > 0) ? n.toFixed(1).replace('.', ',') + ' m³' : '—',
+    format: n => (n > 0) ? n.toFixed(1).replace('.', ',') + ' m³' : '—'
   },
   {
     name: 'Anschluss an eine Fähre',
     find: n => n.ferryNearby,
     reverse: true,
-    format: n => n ? 'ja' : 'nein',
+    format: n => n ? 'ja' : 'nein'
   }
-  /*{
+  /* {
     name: 'Höchster Aufzugschacht',
     find: n => _(n.elevators).map(e => e.wellHeight).filter().max(),
     format: n => (n > 0) ? n.toFixed(2).replace('.', ',') + ' m' : '—',
@@ -76,51 +76,51 @@ let categories = [
     find: n => _(n.elevators).map(e => +e.wellHeight).filter().max(),
     reverse: true,
     format: n => n === -Infinity ? '—' : n.toFixed(2).replace('.', ',') + ' m',
-  },*/
-];
+  }, */
+]
 
-let cards = new Set();
+let cards = new Set()
 
 let potentialCards = categories.map(category => {
-  let filter = category.filter || (() => true);
-  let results = _(stations).filter(filter).sortBy(category.find);
-  if (category.reverse) results = results.reverse();
-  return results.value();
-});
+  let filter = category.filter || (() => true)
+  let results = _(stations).filter(filter).sortBy(category.find)
+  if (category.reverse) results = results.reverse()
+  return results.value()
+})
 
 while (cards.size < potentialCards.length * 4) {
-  let group = cards.size % potentialCards.length;
-  let card = potentialCards[group].shift();
-  cards.add(card);
+  let group = cards.size % potentialCards.length
+  let card = potentialCards[group].shift()
+  cards.add(card)
 }
 
-let jannowitz = _.findWhere(stations, { name: 'Jannowitzbrücke' });
-cards.add(jannowitz);
-cards.delete(_.findWhere(stations, { name: 'Anwanden' }));
+let jannowitz = _.findWhere(stations, { name: 'Jannowitzbrücke' })
+cards.add(jannowitz)
+cards.delete(_.findWhere(stations, { name: 'Anwanden' }))
 
-cards = _(Array.from(cards)).sortBy(s => s.state).value();
+cards = _(Array.from(cards)).sortBy(s => s.state).value()
 
-let i = 0;
+let i = 0
 async.eachLimit(cards, 1, (station, cardDone) => {
-  let cardID = ALPHABET[i / 4 | 0] + (i % 4 + 1);
-  console.log(cardID, station.name);
+  let cardID = ALPHABET[i / 4 | 0] + (i % 4 + 1)
+  console.log(cardID, station.name)
   let card = {
     name: station.name,
     id: cardID,
-    values: [],
-  };
+    values: []
+  }
   categories.forEach(category => {
-    let format = category.format || _.identity;
-    card.values.push({ name: category.name, value: format(category.find(station)) });
-  });
+    let format = category.format || _.identity
+    card.values.push({ name: category.name, value: format(category.find(station)) })
+  })
 
   pdf.add(card).then(cardDone)
   .catch((err) => {
-    console.error(err);
-    cardDone(err);
-  });
-  i++;
+    console.error(err)
+    cardDone(err)
+  })
+  i++
 }, function () {
-  pdf.end();
-  pdf.doc.pipe(fs.createWriteStream(pr(DEST_DIR, 'output.pdf')));
-});
+  pdf.end()
+  pdf.doc.pipe(fs.createWriteStream(pr(DEST_DIR, 'output.pdf')))
+})
--- bin/lib/card-generator.js
+++ bin/lib/card-generator.js
@@ -1,95 +1,95 @@
-'use strict';
+'use strict'
 
-const PDFDocument = require('pdfkit');
-const cheerio = require('cheerio');
-const pr = require('path').resolve;
-const fs = require('fs');
+const PDFDocument = require('pdfkit')
+const cheerio = require('cheerio')
+const pr = require('path').resolve
+const fs = require('fs')
 
-const findImage = require('./find-image');
+const findImage = require('./find-image')
 
 const BLEED = 3
-const WIDTH = 170 + 2 * BLEED;
-const HEIGHT = 255 + 2 * BLEED;
-const MARGIN = 10 + 2 * BLEED;
-const LINE_HEIGHT = 14;
+const WIDTH = 170 + 2 * BLEED
+const HEIGHT = 255 + 2 * BLEED
+const MARGIN = 10 + 2 * BLEED
+const LINE_HEIGHT = 14
 
-const VERKEHRSROT = [ 0, 100, 100, 10 ];
-const LICHTGRAU = [ 0, 0, 0, 20 ];
-const WHITE = [ 0, 0, 0, 0 ];
-const BLACK = [ 0, 0, 0, 100 ];
+const VERKEHRSROT = [ 0, 100, 100, 10 ]
+const LICHTGRAU = [ 0, 0, 0, 20 ]
+const WHITE = [ 0, 0, 0, 0 ]
+const BLACK = [ 0, 0, 0, 100 ]
 
-let $ = cheerio.load(fs.readFileSync(pr(__dirname, '../../src/backside.svg')));
-function drawBacks(doc) {
+let $ = cheerio.load(fs.readFileSync(pr(__dirname, '../../src/backside.svg')))
+function drawBacks (doc) {
   for (let i = 0; i < 9; i++) {
-    let pageX = 595 - (1 + (i / 3 | 0) % 3) * (WIDTH + MARGIN);
-    let pageY = MARGIN + (i % 3) * (HEIGHT + MARGIN);
-    drawBack(doc, pageX, pageY);
+    let pageX = 595 - (1 + (i / 3 | 0) % 3) * (WIDTH + MARGIN)
+    let pageY = MARGIN + (i % 3) * (HEIGHT + MARGIN)
+    drawBack(doc, pageX, pageY)
   }
 }
-function drawBack(doc, pageX, pageY) {
-  doc.save().translate(pageX, pageY);
-  let first = true;
+function drawBack (doc, pageX, pageY) {
+  doc.save().translate(pageX, pageY)
+  let first = true
   $('path').each((i, path) => {
-    let d = $(path).attr('d');
-    doc.path(d);
+    let d = $(path).attr('d')
+    doc.path(d)
     if (first) {
-      first = false;
-      doc.fill(VERKEHRSROT);
+      first = false
+      doc.fill(VERKEHRSROT)
     } else {
-      doc.strokeOpacity(0.5).lineWidth(0.5).stroke(WHITE);
+      doc.strokeOpacity(0.5).lineWidth(0.5).stroke(WHITE)
     }
-  });
+  })
   $('line').each((i, line) => {
-    let $line = $(line);
+    let $line = $(line)
     doc.moveTo($line.attr('x1'), $line.attr('y1'))
       .lineTo($line.attr('x2'), $line.attr('y2'))
-      .strokeOpacity(0.5).lineWidth(0.5).stroke(WHITE);
-  });
-  doc.fontSize(8).fill(WHITE).text('v0.4', MARGIN, MARGIN);
-  doc.restore();
+      .strokeOpacity(0.5).lineWidth(0.5).stroke(WHITE)
+  })
+  doc.fontSize(8).fill(WHITE).text('v0.4', MARGIN, MARGIN)
+  doc.restore()
 }
 
-function makePDF(card) {
-  let doc = new PDFDocument({ size: 'a4', margin: 15 });
-  let i = 0;
+function makePDF (card) {
+  let doc = new PDFDocument({ size: 'a4', margin: 15 })
+  let i = 0
   return {
     doc: doc,
     end: () => {
       if ((i + 1) % 9 !== 0) {
-        doc.addPage();
-        drawBacks(doc);
+        doc.addPage()
+        drawBacks(doc)
       }
-      return doc.end();
+      return doc.end()
     },
     add: (card) => new Promise((resolve, reject) => {
-      let pageX = MARGIN + ((i / 3 | 0) % 3) * (WIDTH + MARGIN);
-      let pageY = MARGIN + (i % 3) * (HEIGHT + MARGIN);
+      let pageX = MARGIN + ((i / 3 | 0) % 3) * (WIDTH + MARGIN)
+      let pageY = MARGIN + (i % 3) * (HEIGHT + MARGIN)
 
       findImage(card).then(image => {
-        let y = 0;
-        doc.rect(pageX + 0, pageY + 0, WIDTH, HEIGHT / 2.5);
-        y = HEIGHT / 2.2;
+        let y = 0
+        doc.rect(pageX + 0, pageY + 0, WIDTH, HEIGHT / 2.5)
+        y = HEIGHT / 2.2
 
         // Declare fonts
-        doc.font(pr(__dirname, '../../src/fonts/FiraSans-Light.ttf'), 'Light');
-        doc.font(pr(__dirname, '../../src/fonts/FiraSans-Book.ttf'), 'Regular');
+        doc.font(pr(__dirname, '../../src/fonts/FiraSans-Light.ttf'), 'Light')
+        doc.font(pr(__dirname, '../../src/fonts/FiraSans-Book.ttf'), 'Regular')
 
-        let placeholderAspectRatio = WIDTH / (HEIGHT / 2.5);
+        let placeholderAspectRatio = WIDTH / (HEIGHT / 2.5)
 
         if (image) {
           // Find out which dimension we need to pass to PDFKit to make sure
           // that the image covers its placeholder.
-          let imageSize;
-          let imageAspectRatio = image.dimensions.width / image.dimensions.height;
+          let imageSize
+          let imageAspectRatio = image.dimensions.width / image.dimensions.height
           if (imageAspectRatio > placeholderAspectRatio) {
-            imageSize = { height: HEIGHT / 2.5 };
+            imageSize = { height: HEIGHT / 2.5 }
           } else {
-            imageSize = { width: WIDTH };
+            imageSize = { width: WIDTH }
           }
           doc.save()
             .clip()
             .image(image.image, pageX + 0, pageY + 0, imageSize)
-            .restore();
+            .restore()
 
           // Attribution
           doc.fontSize(5)
@@ -97,70 +97,70 @@ function makePDF(card) {
             .text(
               image.metadata.Author || image.metadata.url,
               pageX + MARGIN / 2 + 2,
-              pageY + HEIGHT / 2.5 - 8);
+              pageY + HEIGHT / 2.5 - 8)
         } else {
-          doc.fill(LICHTGRAU);
+          doc.fill(LICHTGRAU)
         }
 
         doc.moveTo(pageX + 0, pageY + HEIGHT / 2.5 + 4)
           .lineTo(pageX + WIDTH, pageY + HEIGHT / 2.5 + 4)
           .lineWidth(3)
           .strokeOpacity(1)
-          .stroke(VERKEHRSROT);
+          .stroke(VERKEHRSROT)
 
-        doc.fill(BLACK);
-        doc.circle(pageX + WIDTH - MARGIN - 5, pageY + MARGIN + 7, 10).fill(BLACK);
-        doc.fontSize(12);
-        doc.font('Light').fill(WHITE).text(card.id, pageX - 2 * (MARGIN + BLEED) + WIDTH - 23, pageY + MARGIN, { width: 80, align: 'center' });
+        doc.fill(BLACK)
+        doc.circle(pageX + WIDTH - MARGIN - 5, pageY + MARGIN + 7, 10).fill(BLACK)
+        doc.fontSize(12)
+        doc.font('Light').fill(WHITE).text(card.id, pageX - 2 * (MARGIN + BLEED) + WIDTH - 23, pageY + MARGIN, { width: 80, align: 'center' })
 
-        doc.fontSize(9);
-        doc.font('Regular').fill(BLACK).text(card.name, pageX + MARGIN, pageY + y);
-        y += LINE_HEIGHT * 1.5;
+        doc.fontSize(9)
+        doc.font('Regular').fill(BLACK).text(card.name, pageX + MARGIN, pageY + y)
+        y += LINE_HEIGHT * 1.5
 
-        doc.fontSize(8);
+        doc.fontSize(8)
         card.values.forEach(category => {
-          doc.font('Light').text(category.name, pageX + MARGIN, pageY + y);
-          doc.font('Regular').text(category.value, pageX + MARGIN, pageY + y, { width: WIDTH - 2 * MARGIN, align: 'right' });
-          y += LINE_HEIGHT;
-        });
+          doc.font('Light').text(category.name, pageX + MARGIN, pageY + y)
+          doc.font('Regular').text(category.value, pageX + MARGIN, pageY + y, { width: WIDTH - 2 * MARGIN, align: 'right' })
+          y += LINE_HEIGHT
+        })
 
-        doc.fontSize(8);
+        doc.fontSize(8)
 
-        doc.lineWidth(0.5);
+        doc.lineWidth(0.5)
 
         // Draw cutting marks
         doc.moveTo(pageX + BLEED, pageY - 6)
           .lineTo(pageX + BLEED, pageY + BLEED)
           .lineTo(pageX - 6, pageY + BLEED)
-          .stroke(BLACK);
+          .stroke(BLACK)
         doc.moveTo(pageX - BLEED + WIDTH, pageY - 6)
           .lineTo(pageX - BLEED + WIDTH, pageY + BLEED)
           .lineTo(pageX + WIDTH + 6, pageY + BLEED)
-          .stroke();
+          .stroke()
         doc.moveTo(pageX - BLEED + WIDTH, pageY + BLEED + HEIGHT + 6)
           .lineTo(pageX - BLEED + WIDTH, pageY + HEIGHT)
           .lineTo(pageX + WIDTH + 6, pageY + HEIGHT)
-          .stroke();
+          .stroke()
         doc.moveTo(pageX + BLEED, pageY + BLEED + HEIGHT + 6)
           .lineTo(pageX + BLEED, pageY + HEIGHT)
           .lineTo(pageX - 6, pageY + HEIGHT)
-          .stroke();
+          .stroke()
 
         doc.rect(pageX + BLEED, pageY + BLEED, WIDTH - 2 * BLEED, HEIGHT - 2 * BLEED + 3)
           .lineWidth(0.25)
-          .stroke(BLACK);
+          .stroke(BLACK)
 
-        i++;
+        i++
         if (i % 9 === 0) {
-          doc.addPage();
-          drawBacks(doc);
-          doc.addPage();
+          doc.addPage()
+          drawBacks(doc)
+          doc.addPage()
         }
 
-        resolve();
-      }).catch(reject);
-    }),
+        resolve()
+      }).catch(reject)
+    })
   }
 }
 
-module.exports = makePDF;
+module.exports = makePDF
